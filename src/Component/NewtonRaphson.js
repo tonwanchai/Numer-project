d753caedbee3442d2f68d984618d6f87fc69ca56
tonwanchai <grand38326@gmail.com>
2021-05-26 14:19:16 +0700

    Update NewtonRaphson.js

--- src/Component/NewtonRaphson.js
+++ src/Component/NewtonRaphson.js
@@ -21,14 +21,11 @@ class NewtonRaphson extends Component {
         let tmpData = null
         await apis.getAllRootOfEquation().then(res => (tmpData = res.data));
         this.setState({ apiData: tmpData })
-        console.log(this.state.apiData);
-        let n = this.state.apiData.length;
-        console.log(n);
-        let ranIndex = Math.floor(Math.random() * n);
+
         this.setState({
-            f_x: this.state.apiData[ranIndex]["equation"],
-            init_x: this.state.apiData[ranIndex]["initial_x"],
-            er: this.state.apiData[ranIndex]["error"],
+            f_x: this.state.apiData[3]["equation"],
+            init_x: this.state.apiData[3]["initial_x"],
+            er: this.state.apiData[3]["error"],
         })
 
     }

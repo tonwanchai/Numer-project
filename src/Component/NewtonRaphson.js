fd75126c16aed15bcfff6de52cec88af93849fb6
tonwanchai <grand38326@gmail.com>
2021-05-28 14:28:19 +0700

    Update NewtonRaphson.js

--- src/Component/NewtonRaphson.js
+++ src/Component/NewtonRaphson.js
@@ -3,7 +3,7 @@ import { equation_func, fixed_fx } from './Equation_Function'
 import { Input, Button } from 'antd';
 import apis from '../Container/API'
 import { create, all } from 'mathjs'
-
+import functionPlot from 'function-plot'
 const config = {}
 const math = create(all, config)
 
@@ -15,7 +15,8 @@ class NewtonRaphson extends Component {
         er: null,
         ans: null,
         apiData: null,
-        ifer: null
+        ifer: null,
+        tmpX: [[]]
     }
     async GetDatafromAPI() {
         let tmpData = null
@@ -59,12 +60,16 @@ class NewtonRaphson extends Component {
             let x_new;
             f_x = fixed_fx(f_x);
             De_f_x = fixed_fx(De_f_x);
+            let tmpX = [];
             while (tmp_er > er) {
 
                 x_new = x - (equation_func(x, f_x) / equation_func(x, De_f_x));
                 tmp_er = Math.abs((x_new - x) / x_new);
                 x = x_new;
-
+                
+                tmpX.push([])
+                tmpX[i-1].push(x)
+                tmpX[i-1].push(equation_func(x,f_x))
                 arr.push(<div style={{ fontSize: '25px' }}>
                     <span style={{ display: 'inline-block', width: '50%' }}>Iteration {i}: x is {x}</span>
                     <span>Error : {tmp_er.toFixed(15)}</span>
@@ -77,10 +82,39 @@ class NewtonRaphson extends Component {
 
             arr.push(<div style={{ fontSize: '40px', fontWeight: 'bold' }}>Result of x is {x}</div>);
 
-            this.setState({ ans: arr })
+            this.setState({ ans: arr ,tmpX:tmpX})
         } catch (error) {
             this.setState({ ifer: (<div style={{ color: 'red' }}>ใส่ฟังก์ชั่นไม่ถูกต้อง</div>) })
         }
+    }
+    Plot() {
+        console.log(this.state.tmpX)
+        
+        functionPlot({
+            target: "#plt",
+            width: 700,
+            height: 700, 
+            xAxis: { domain: [-5, 5] },
+            yAxis: { domain: [-1, 9] },
+            grid: true,
+            data: [
+                {
+                    fn: this.state.f_x
+                },           
+                {
+
+                    points: this.state.tmpX,
+                    fnType: 'points',
+                    graphType: 'scatter'
+
+                }
+
+            ]
+        });
+    }
+    onClickShow = e =>{
+        this.Plot();
+        console.log("onclickshow")
     }
     render() {
         return (
@@ -99,13 +133,15 @@ class NewtonRaphson extends Component {
 
                 </div>
                 <div>
-                    <Button style={{ marginLeft: '5px', width: '100px', marginTop: '5px' }} type='primary' onClick={this.onClickExample}>Example</Button>
+                <span><Button style={{ marginLeft: '5px', width: '100px', marginTop: '5px' }} type='primary' onClick={this.onClickExample}>Example</Button></span>
+                    
+                    <span><Button style={{ marginLeft: '5px', width: '100px', marginTop: '5px' }} type='primary' onClick={this.onClickShow}>Show</Button></span> 
 
                 </div>
                 <div style={{ marginTop: '20px' }}>
                     {this.state.ans}
                 </div>
-
+                <div id="plt" style={{margin:"20px"}} />
             </div>
         )
 
